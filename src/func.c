568eadb3ddfcae0fbc76fdf3c62f8ffc8c8ced47
Драганчук Алена Сергеевна <ri57s05@awp09.csc.local>
2016-04-12 10:01:16 +0600

    codestyle

--- src/func.c
+++ src/func.c
@@ -1,25 +1,26 @@
 #include<stdio.h>
 #include "func.h"
+
 int sum(int arg1, int arg2){
 	int rez;
 	rez = arg1 + arg2;
-return rez;
+	return rez;
 }
 
 int raz(int arg1, int arg2){
 	int rez;
 	rez = arg1 - arg2;
-return rez;
+	return rez;
 }
 
 int del(int arg1, int arg2){
 	int rez;
 	rez = arg1 / arg2;
-return rez;
+	return rez;
 }
 
 int proz(int arg1, int arg2){
 	int rez;
 	rez = arg1 * arg2;
-return rez;
+	return rez;
 }
